fb5c5a5a1711c9d5914d7f705258bd65221cdcf5
Nathiel12 <nathiel_calderon@ucne.edu.do>
2025-10-01 21:14:24 -0600

    Cambiado precio y cantidad changed a string

--- app/src/main/java/edu/ucne/nathiel_taveras_ap2_p1/Presentation/Edit/EditEntradasUiEvent.kt
+++ app/src/main/java/edu/ucne/nathiel_taveras_ap2_p1/Presentation/Edit/EditEntradasUiEvent.kt
@@ -4,7 +4,7 @@ sealed interface EditEntradasUiEvent {
     data class Load(val id: Int?) : EditEntradasUiEvent
     data class NombreClienteChanged(val value: String) : EditEntradasUiEvent
     data class FechaChanged(val value: String) : EditEntradasUiEvent
-    data class PrecioChanged(val value: Double) : EditEntradasUiEvent
+    data class PrecioChanged(val value: String) : EditEntradasUiEvent
     data class CantidadChanged(val value: String) : EditEntradasUiEvent
     data object Save : EditEntradasUiEvent
     data object Delete : EditEntradasUiEvent
